d97c96ece23618cf42f056ac4134308de0590825
Dmitry Weiner <dmitry.weiner@gmail.com>
2022-05-04 14:45:07 +0700

    Implement JS from https://stackoverflow.com/a/53058938/3012961

--- app/src/main/java/com/example/run_js_android/MainActivity.java
+++ app/src/main/java/com/example/run_js_android/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.run_js_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import android.util.Log;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        EditText input = findViewById(R.id.editTextInput);
+        EditText output = findViewById(R.id.editTextOutput);
+
+        Context context = Context.enter(); //
+        context.setOptimizationLevel(-1); // this is required[2]
+        Scriptable scope = context.initStandardObjects();
+        Object result = context.evaluateString(scope, input.getText().toString(), "<cmd>", 1, null);
+        output.setText(result.toString());
     }
 }
